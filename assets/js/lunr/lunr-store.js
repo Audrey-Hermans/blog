d18f1f1c825034ddad29af5362a1850a906cf654
jmesters <jmesters@users.noreply.github.com>
2025-04-07 19:05:20 +0000

    deploy: e8fda7a1714fd2cadb95db5d37386e87a99c5a66

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "My First Blog Post",
         "excerpt":"🎉 This is my very first blog post on my Jekyll site using the Minimal Mistakes theme!   I’m writing this to test everything and start documenting my journey 🚀  ","categories": [],
         "tags": [],
-        "url": "/Website-2025/my-first-post/",
+        "url": "/blog/my-first-post/",
         "teaser": null
       }]
